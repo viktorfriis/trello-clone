b22666d7aa3e28ed89b0fc51472dc351037a0bd5
viktor kjeldal <vikt5485@stud.kea.dk>
2020-04-17 19:40:39 +0200

    cards are now moveable between stages, and can be added from form

--- script.js
+++ script.js
@@ -7,26 +7,29 @@ const todoContainer = document.querySelector("#todo .cards");
 const progressContainer = document.querySelector("#progress .cards");
 const doneContainer = document.querySelector("#done .cards");
 const APIKey = "5e957b2e436377171a0c2346";
+const form = document.querySelector("form");
+const elements = form.elements;
 
 function start() {
     console.log("start");
     get();
-}
 
-window.addEventListener("load", () => {
-    document.querySelector(".add-new").addEventListener("click", () => {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
         const data = {
-            title: document.querySelector("#inp-title").value,
-            description: document.querySelector("#inp-desc").value,
-            creator: document.querySelector("#inp-creator").value,
-            estimate: document.querySelector("#inp-estimate").value,
-            deadline: document.querySelector("#inp-deadline").value,
-            priority: document.querySelector("#inp-priority").value,
+            title: elements.title.value,
+            description: elements.description.value,
+            creator: elements.creator.value,
+            estimate: elements.estimate.value,
+            deadline: elements.deadline.value,
+            priority: elements.priority.value,
             dest: "todo"
         };
+
         post(data);
     })
-})
+}
 
 //GET
 function get() {
@@ -96,20 +99,93 @@ function showCard(e) {
     clone.querySelector(".title").textContent = e.title;
     clone.querySelector(".desc").textContent = e.description;
     clone.querySelector(".creator").textContent += e.creator;
-    clone.querySelector(".estimate span").textContent = " " + e.estimate + "h";
-    clone.querySelector(".deadline span").textContent = " " + formatDate(e);
-    clone.querySelector(".priority span").textContent = " " + e.priority;
+    clone.querySelector(".estimate span+span").textContent = " " + e.estimate + "h";
+    clone.querySelector(".deadline span+span").textContent = " " + formatDate(e);
+    clone.querySelector(".priority span+span").textContent = " " + e.priority;
 
 
     // clone.querySelector("button.update-this").addEventListener("click", () => {
     //     put(e._id);
     // })
 
+    if (dest === "done") {
+        clone.querySelector(".move-left").classList.add("visible");
+        clone.querySelector(".move-left").classList.remove("hidden");
+        clone.querySelector(".move-right").classList.add("hidden");
+        clone.querySelector(".move-right").classList.remove("visible");
+
+        clone.querySelector(".move-left").addEventListener("click", () => {
+            updateDest(e, "left");
+        });
+    } else if (dest === "progress") {
+        clone.querySelector(".move-left").classList.add("visible");
+        clone.querySelector(".move-left").classList.remove("hidden");
+        clone.querySelector(".move-right").classList.add("visible");
+        clone.querySelector(".move-right").classList.remove("hidden");
+
+
+        clone.querySelector(".move-left").addEventListener("click", () => {
+            updateDest(e, "left");
+        });
+
+        clone.querySelector(".move-right").addEventListener("click", () => {
+            updateDest(e, "right");
+        });
+    } else if (dest === "todo") {
+        clone.querySelector(".move-left").classList.add("hidden");
+        clone.querySelector(".move-left").classList.remove("visible");
+        clone.querySelector(".move-right").classList.add("visible");
+        clone.querySelector(".move-right").classList.remove("hidden");
+
+        clone.querySelector(".move-right").addEventListener("click", () => {
+            updateDest(e, "right");
+        });
+    }
+
     clone.querySelector("button.delete-this").addEventListener("click", () => {
         deleteIt(e._id);
     })
 
     document.querySelector(`#${dest} .cards`).appendChild(clone);
+}
+
+function updateDest(e, dir) {
+    let currentDest = e.dest;
+    let newDest;
+    let id = e._id;
+
+    if (currentDest === "todo" && dir === "right" || currentDest === "done" && dir === "left") {
+        newDest = "progress";
+    } else if (currentDest === "progress" && dir === "left") {
+        newDest = "todo";
+    } else if (currentDest === "progress" && dir === "right") {
+        newDest = "done";
+    }
+
+    const data = {
+        dest: newDest
+    };
+
+    document.querySelector(`article[data-id="${id}"]`).remove();
+
+    put(id, data);
+}
+
+//PUT 
+function put(id, data) {
+    const postData = JSON.stringify(data);
+
+    fetch(endPoint + "/" + id, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "x-apikey": `${APIKey}`,
+                "cache-control": "no-cache"
+            },
+            body: postData
+        })
+        .then(e => e.json())
+        .then(e => showCard(e));
 }
 
 function formatDate(e) {
